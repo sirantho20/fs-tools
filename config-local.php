775360648f234db99674393049840485612378ab
Anthony <aafetsrom@htghana.com>
2015-02-18 15:07:43 +0000

    vendor and other files removed

--- config-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'email_to' => 'aafetsrom@softcube.co',
-    'smtp_host' =>'smtp.gmail.com',
-    'smtp_user' =>'sirantho20@gmail.com',
-    'smtp_password' =>'afTONY19833',
-    'smtp_port' => 587,
-];
